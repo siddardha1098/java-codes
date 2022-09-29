9fe25996e2e2e1a2f031384569ead8ca5006047b
siddardha1098 <112462514+siddardha1098@users.noreply.github.com>
2022-09-29 13:33:54 +0530

    Add files via upload

---
+++ NumberofFactors.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Factors
+{
+public static void main(String[] agrs)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("ENTER A NUMBER");
+        int n=sc.nextInt();
+        int i,count=0;
+        for(i=1;i<=n;i++)
+        {
+            if(n%i==0)
+            count++; 
+        }
+        System.out.print("NUMBER OF FACTORS OF "+n+" IS "+count);
+    }
+}
---
+++ squareandcube.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class SquareandCube
+ {
+    public static void main(String args[])
+ {
+        Scanner sc = new Scanner(System.in);
+        double num;
+
+        System.out.print("Enter an integer number: ");
+        num = sc.nextDouble();
+
+        System.out.println("Square of " + num + " is: " + Math.pow(num, 2));
+        System.out.println("Cube of " + num + " is: " + Math.pow(num, 3));
+    }
+}
